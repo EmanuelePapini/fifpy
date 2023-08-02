67b869a3d3ee1f3dcfc8dae2efa65f9e952e2ff7
Emanuele Papini <emanuele_papini@csesINAF01.iaps.inaf.it>
2023-08-02 11:51:07 +0000

    fifpy4RB merged with fifpy

--- test_MvIF.py
+++ test_MvIF.py
@@ -1,5 +1,5 @@
 import numpy as np
-from . import IF 
+from . import MvIF 
 import pylab as plt
 from . import time
 
@@ -10,6 +10,8 @@ if profile:
 timeit = time.timeit()
 plt.ion()
 
+import numpy as np
+from . import MvIF
 n=30000
 size=11
 sigma=3
@@ -19,11 +21,16 @@ y2 = np.sin(2*x)
 y1 = np.cos(10*x+2.3)
 y = np.sin(2*x) + np.cos(10*x+2.3)
 
-fif = IF(verbose=True,Xi=2.0,ExtPoints=9)
+z2 = np.cos(4*x)
+z1 = np.sin(12*x+1.2)
+z = z1 + z2
+
+sig = np.vstack((y,z))
+fif = MvIF(verbose=True,Xi=2.0,ExtPoints=9)
 if profile:
     timeit.tic; cProfile.run("fif.run(y)",'IF_profiled'); timeit.toc
 else:
-    timeit.tic; fif.run(y); timeit.toc
+    timeit.tic; fif.run(sig); timeit.toc
 #y = np.zeros(n)
 #
 #y[n//2] = 1
@@ -32,12 +39,17 @@ else:
 #(convhigh,SD) = convolve_high(y,a)
 IMF = fif.IMC
 
-fig,ax = plt.subplots(IMF.shape[0]+1)
-ax[0].plot(y)
-for i in range(IMF.shape[0]):
-    ax[i+1].plot(IMF[i])
-#[plt.plot(i) for i in IMF]
-ax[1].plot(y1,'--')
-ax[2].plot(y2,'--')
+fig,ax = plt.subplots(IMF.shape[0]+1,2)
+ax = ax.transpose()
+ax[0,0].plot(y)
+ax[1,0].plot(z)
 
+for i in range(IMF.shape[0]):
+    ax[0,i+1].plot(IMF[i,0])
+    ax[1,i+1].plot(IMF[i,1])
+#[plt.plot(i) for i in IMF]
+ax[0,1].plot(y1,'--')
+ax[0,2].plot(y2,'--')
+ax[1,1].plot(z1,'--')
+ax[1,2].plot(z2,'--')
 
