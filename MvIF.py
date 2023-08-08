fixed MvIF freq. calculation

MvIF.py
@@ -161,7 +161,7 @@ def MvIF(f,options,M=np.array([]), window_file=None, data_mask = None, nthreads 
     ### Begin Iterating ###
     while countIMFs < opts.NIMFs and k_pp >= opts.ExtPoints:
         countIMFs += 1
-        print('IMF', countIMFs)
+        #print('IMF', countIMFs)
         
         h = f
         if 'M' not in locals() or np.size(M)<countIMFs:

MvIFpy.py
@@ -41,6 +41,7 @@ def Settings(**kwargs):
     # General 
     #options['saveEnd'] = 0
     #options['saveInter'] = 0
+    options['silent'] = False    
     options['verbose'] = False    
     options['timeit'] = False    
     #options['plots'] = 0.0
@@ -90,6 +91,7 @@ def MvIF(in_f,options,M=np.array([]), window_file=None, data_mask = None, nthrea
         TO BE IMPLEMENTED
     """
     opts = AttrDictSens(options)
+    silent = opts.silent
     if nthreads is not None:
         if opts.imf_method == 'numba': 
             set_num_threads(nthreads)
@@ -109,7 +111,7 @@ def MvIF(in_f,options,M=np.array([]), window_file=None, data_mask = None, nthrea
         compute_imf = compute_imf_numba
 
     if opts.MaskLengthType == 'amp': 
-        print('using amplitude to calculate mask')
+        if not silent: print('using amplitude to calculate mask')
         tol = 1e-18
     #loading master filter
     ift = opts.timeit
@@ -161,7 +163,7 @@ def MvIF(in_f,options,M=np.array([]), window_file=None, data_mask = None, nthrea
     ### Begin Iterating ###
     while countIMFs < opts.NIMFs and k_pp >= opts.ExtPoints:
         countIMFs += 1
-        print('IMF', countIMFs)
+        if not silent: print('IMF', countIMFs)
         
         h = np.copy(f)
         if 'M' not in locals() or np.size(M)<countIMFs:
@@ -196,7 +198,7 @@ def MvIF(in_f,options,M=np.array([]), window_file=None, data_mask = None, nthrea
         if ift: time_imfs += ttime.get_toc
         
         if inStepN >= opts.MaxInner:
-            print('Max # of inner steps reached')
+            if not silent: print('Max # of inner steps reached')
 
         #stats['inStepN'].append(inStepN)
         stats_list[countIMFs-1].inStepN = inStepN

__init__.py
@@ -217,6 +217,10 @@ class MvFIF(FIF):
    
         #contains ancillary data which keep trace of the processing done on the data
         self.ancillary = {}
+    
+    @property
+    def IMC(self):
+        return self.data['IMC'][:,:,self.wsh:-self.wsh] if self.wsh >0 else self.data['IMC'] 
 
 
     def get_inst_freq_amp(self,dt, as_output = False ):
@@ -250,15 +254,31 @@ class MvFIF(FIF):
                 
         """
         wsh = self.ancillary['wshrink']
-
-        self.data['freqs'], self.data['amps'] = ftools.IMC_get_freq_amp(np.squeeze(self.data['IMC'][:,0,:]), \
+        
+        nimf,ndim,nt = self.data['IMC'].shape
+        freqs = np.zeros((ndim,nimf))
+        amps = np.zeros((ndim,nimf))
+        for i in range(ndim):
+            freqt,ampt = ftools.IMC_get_freq_amp(self.data['IMC'][:,i,:], \
                      use_instantaneous_freq = use_instantaneous_freq, wshrink = wsh, \
                      **kwargs)
-
+            freqs[i] = freqt
+            amps[i] = ampt
+        self.data['freqs'] = freqs 
+        self.data['amps'] = amps
         self.ancillary['get_freq_amplitudes'] = kwargs
         self.ancillary['get_freq_amplitudes']['use_instantaneous_freq'] = use_instantaneous_freq
         
         if as_output: return self.data['freqs'], self.data['amps']
+
+    def orthogonalize(self,threshold = 0.6, only_nearest = True, **kwargs):
+        
+        if self.data['IMC'].shape[0] <3: #if shape==2 then only one imf was extracted
+            return
+        IMCs = self.data['IMC']
+        imfs = ftools.orthogonalize_MvFIF(IMCs,threshold, only_nearest, **kwargs)
+        self.ancillary['orthogonalized'] = True
+        self.data['IMC'] = imfs
 
 class IF(FIF):
     """
@@ -350,9 +370,10 @@ class MvIF(MvFIF):
         preprocess : str
             allowed values : 'make-periodic', 'extend-periodic', None
         """
+        silent = self.options['silent']
         D,N = np.shape(in_f)
         if preprocess == 'make-periodic':
-            print('\nmaking input signal periodic...')
+            if not silent: print('\nmaking input signal periodic...')
             from .arrays import make_periodic
             
             if wshrink == 0 : wshrink = in_f.size//4 
@@ -361,7 +382,7 @@ class MvIF(MvFIF):
             for iD in range(D):    
                 out_f[iD] = make_periodic(in_f[iD],wshrink)
         elif preprocess == 'extend-periodic':
-            print('\nextending input signal (asymmetric-periodic)...')
+            if not silent: print('\nextending input signal (asymmetric-periodic)...')
 
             from .arrays import extend_signal
             
@@ -371,7 +392,7 @@ class MvIF(MvFIF):
             out_f = np.zeros((D,ff.size))
             out_f[0] = ff
             for iD in range(1,D):
-                out_f[iD] = extend_signal(in_f[i],wshrink,npad_raisedcos = npad_raisedcos) 
+                out_f[iD] = extend_signal(in_f[iD],wshrink,npad_raisedcos = npad_raisedcos) 
 
             if data_mask is not None:
                 data_mask = extend_signal(data_mask,wshrink,mode='reflect')