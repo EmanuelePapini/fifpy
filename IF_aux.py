bugs fixed in MIF and added calculation of frequencies

IF_aux.py
@@ -394,8 +394,8 @@ def find_max_frequency2D(f,nsamples = 1, **kwargs):
     
     """
     from random import randrange
-    maxmins_x = []
-    maxmins_y = []
+    #maxmins_x = []
+    #maxmins_y = []
     N,M = f.shape
     kn = N//nsamples
     km = M//nsamples
@@ -406,19 +406,20 @@ def find_max_frequency2D(f,nsamples = 1, **kwargs):
         
         #this is to avoid that None are produced
         for ix in range(kn):
-            mxms = Maxmins(f[randrange(N),:].flatten(),**kwargs)
-            if mxms is not None: break
-        maxmins_x.append(mxms)
-        del mxms
+            maxmins_x = Maxmins(f[randrange(N),:].flatten(),**kwargs)
+            if maxmins_x is not None: break
+        #maxmins_x.append(mxms)
+        #del mxms
         for iy in range(km):
-            mxms = Maxmins(f[:,randrange(M)].flatten(),**kwargs)
-            if mxms is not None: break
-        maxmins_y.append(mxms)
-        del mxms
-        if maxmins_x[-1] is not None and maxmins_y[-1] is not None:
-            diff_x = [] if len(maxmins_x[-1]) < 1 else np.diff(maxmins_x[-1])
-            diff_y = [] if len(maxmins_y[-1]) < 1 else np.diff(maxmins_y[-1])
-            k_pp.append([maxmins_x[-1].shape[0],maxmins_y[-1].shape[0]]) 
+            maxmins_y = Maxmins(f[:,randrange(M)].flatten(),**kwargs)
+            if maxmins_y is not None: break
+        #maxmins_y.append(mxms)
+        #del mxms
+        if maxmins_x is not None and maxmins_y is not None:
+            diff_x = [] if np.size(maxmins_x) < 2 else np.diff(maxmins_x)
+            diff_y = [] if np.size(maxmins_y) < 2 else np.diff(maxmins_y)
+            
+            k_pp.append([np.size(maxmins_x),np.size(maxmins_y)]) 
             diffMaxmins_x.append(diff_x) 
             diffMaxmins_y.append(diff_y) 
     if np.sum(k_pp) == 0.:
@@ -432,8 +433,8 @@ def find_max_frequency2D(f,nsamples = 1, **kwargs):
     #calculation (see get_mask_length
     #diffMaxmins_x = np.array(diffMaxmins_x).flatten()
     #diffMaxmins_y = np.array(diffMaxmins_y).flatten()
-    diffMaxmins_x = np.concatenate([i.flatten() for i in diffMaxmins_x if i.size > 0])
-    diffMaxmins_y = np.concatenate([i.flatten() for i in diffMaxmins_y if i.size > 0])
+    diffMaxmins_x = np.concatenate([i.flatten() for i in diffMaxmins_x if np.size(i) > 0])
+    diffMaxmins_y = np.concatenate([i.flatten() for i in diffMaxmins_y if np.size(i) > 0])
     N_pp = (N,M)
 
     return N_pp, k_pp, diffMaxmins_x,diffMaxmins_y

MIFpy.py
@@ -153,13 +153,20 @@ def MIF_v3e(in_f,options,M=np.array([]), window_mask = None, data_mask = None, n
 
     #NOW starting the calculation of the IMFs
 
+    countIMFs = 0
     #Find max-frequency contained in signal
     if ift: ttime.tic 
-    N_pp, k_pp, diffMaxmins_x, diffMaxmins_y = \
-        find_max_frequency2D(f,opts['Maxmins_samples'],tol=tol, mode = opts.BCmode, method = opts.Maxmins_method)
+    
+    if 'M' not in locals() or np.size(M) == 0:
+        N_pp, k_pp, diffMaxmins_x, diffMaxmins_y = \
+            find_max_frequency2D(f,opts['Maxmins_samples'],tol=tol,\
+            mode = opts.BCmode, method = opts.Maxmins_method)
+    else:
+        k_pp = [opts.ExtPoints*2] #[int(np.max([nx,ny])/M[countIMFs])]
+        #k_pp = [int(np.max([nx,ny])/M[0])]
+    
     if ift: time_max_nu += ttime.get_toc
 
-    countIMFs = 0
     stats_list = np.recarray(opts.NIMFs,dtype=[('logMx',int),('logMy',int),('inStepN',int)])
     stats_list.logM = 0
     stats_list.inStepN = 0
@@ -172,12 +179,15 @@ def MIF_v3e(in_f,options,M=np.array([]), window_mask = None, data_mask = None, n
         h = np.copy(f)
         if 'M' not in locals() or np.size(M)<countIMFs:
             m = get_mask_length2D(opts,N_pp,k_pp,diffMaxmins_x,diffMaxmins_y,logM,countIMFs)
+            if opts.verbose:
+                print('\n IMF # %1.0d   -   # Extreme points (%s\n' %(countIMFs,k_pp))
         else:
             m = M[countIMFs-1]
-            if type(m) is int : m = (m)*2 
+            if type(m) is not tuple : m = (m,)*2 
+            if opts.verbose:
+                print('\n IMF # %1.0d   -   # mask length (%s\n' %(countIMFs,m))
 
         if opts.verbose:
-            print('\n IMF # %1.0d   -   # Extreme points (%s\n' %(countIMFs,k_pp))
             print('\n  step #            SD             Mask length \n\n')
 
         #stats = {'logM': [], 'inStepN': []}
@@ -214,10 +224,14 @@ def MIF_v3e(in_f,options,M=np.array([]), window_mask = None, data_mask = None, n
         f = f - h
     
         #Find max-frequency contained in residual signal
+        
         if ift: ttime.tic 
-        N_pp, k_pp, diffMaxmins_x, diffMaxmins_y = \
-          find_max_frequency2D(f,opts['Maxmins_samples'],tol=tol, \
-            mode = opts.BCmode, method = opts.Maxmins_method)
+        if 'M' not in locals() or np.size(M)<countIMFs+1:
+            N_pp, k_pp, diffMaxmins_x, diffMaxmins_y = \
+                find_max_frequency2D(f,opts['Maxmins_samples'],tol=tol, \
+                mode = opts.BCmode, method = opts.Maxmins_method)
+        else:
+            k_pp = [opts.ExtPoints*2] #[int(np.max([nx,ny])/M[countIMFs])]
         if ift: time_max_nu += ttime.get_toc
         
 