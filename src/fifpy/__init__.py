7d92f620f95ac9f875d5b9592e7bb1cfc4d338c9
Emanuele Papini <papini@sagittarius.localdomain>
2024-11-16 09:35:18 +0100

    workaround for fixing attrdict incompatibility with collection

--- src/fifpy/__init__.py
+++ src/fifpy/__init__.py
@@ -7,7 +7,11 @@
     Python version: Emanuele Papini - INAF (emanuele.papini@inaf.it) 
     Original matlab version: Antonio Cicone - university of L'Aquila (antonio.cicone@univaq.it)
 """
-
+# Monkey patch collections to fix incompatibility with attrdict
+import collections
+import collections.abc
+for type_name in collections.abc.__all__:
+    setattr(collections, type_name, getattr(collections.abc, type_name))
 
 
 from . import fif_tools as ftools
