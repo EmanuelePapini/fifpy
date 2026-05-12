added few optimizations for faster calculation. Added calculate istantaneous frequency for MvIF. Minor fixes.

src/fifpy/IF_aux.py
@@ -14,6 +14,10 @@ from numpy import linalg as LA
 from scipy.signal import argrelextrema 
 from numba import njit,prange,get_num_threads,set_num_threads
 
+import pyfftw
+import pyfftw.interfaces.numpy_fft as fft
+
+pyfftw.interfaces.cache.enable()
 
 from .prefixed_double_filter import MM as FKmask
 FKmask = np.array(FKmask)
@@ -316,7 +320,7 @@ def compute_imf_numba(f,a,options):
 
     h_ave, inStepN, SD = iterate_numba(h,h_ave,a,options.delta,options.MaxInner)
     
-    if options.verbose:
+    if not options.silent:
         print('(numba): %2.0d      %1.40f          %2.0d\n' % (inStepN, SD, np.size(a)))
 
     return h_ave,inStepN,SD
@@ -359,10 +363,14 @@ def compute_imf_fft(f,a,options):
     SD = 1.
     
     Nh = len(h)
+    if BCmod == 'wrap':
+        ker_fft = precompute_kernel_fft(kernel, h.shape[0])
+    
     while SD>delta and inStepN<MaxInner:
         inStepN += 1
         if BCmod == 'wrap':
-            h_ave = fftconvolve1D(h,kernel)
+            h_ave = fftconvolve1D_kerfft(h,ker_fft,kernel.shape[0])
+            # h_ave = fftconvolve1D_kerfft(h,kernel)
         else:
             h_ave = fftconvolve(h,kernel,mode='same')
         #computing norm
@@ -374,11 +382,102 @@ def compute_imf_fft(f,a,options):
     
 
     
-    if options.verbose:
+    if not options.silent:
         print('(fft): %2.0d      %1.40f          %2.0d\n' % (inStepN, SD, np.size(a)))
 
     return h,inStepN,SD
 
+
+@njit
+def _fast_norm_sq(arr):
+    """Compute squared L2 norm efficiently using numba."""
+    norm_sq = 0.0
+    for i in range(len(arr)):
+        norm_sq += arr[i] * arr[i]
+    return norm_sq
+
+
+@njit
+def _fast_subtract(h, h_ave):
+    """Subtract h_ave from h in-place and return updated h."""
+    for i in range(len(h)):
+        h[i] -= h_ave[i]
+    return h
+
+
+def compute_imf_fft_numba(f, a, options):
+    """
+    Fast version of compute_imf_fft using numba JIT compilation for norm calculations.
+    
+    Extracts the imf from the signal f using the window function (mask) a,
+    according to the settings specified in the options dict.
+    
+    This version replaces scipy.linalg.norm with a numba-compiled function for ~10x speedup
+    on the bottleneck norm calculations.
+    
+    N.B. This calculation is done via convolution of f with a in Fourier space,
+    using scipy.signal.fftconvolve or pyfftw's FFT operations.
+    
+    Parameters
+    ----------
+    f : 1D float array
+        input signal
+    a : 1D float array
+        window function
+    
+    options : dict
+        dictionary containing the settings of the decomposition 
+        (see, e.g.,  fifpy.IFpy.Settings method).
+        mandatory keyword in options:
+        'delta' : minimum difference in the 2norm between the two iterations
+        'MaxInner': maximum number of iterations
+        'verbose' : verbosity level 
+    """
+    from scipy.signal import fftconvolve
+
+    h = np.array(f, copy=True)
+    kernel = a
+    delta = options.delta
+    MaxInner = options.MaxInner
+    BCmod = options.BCmode
+    verbose = options.verbose
+    
+    inStepN = 0
+    SD = 1.0
+    
+    # Precompute kernel FFT for wrap mode
+    if BCmod == 'wrap':
+        ker_fft = precompute_kernel_fft(kernel, h.shape[0])
+    
+    while SD > delta and inStepN < MaxInner:
+        inStepN += 1
+        
+        # Compute convolution
+        if BCmod == 'wrap':
+            h_ave = fftconvolve1D_kerfft(h, ker_fft, kernel.shape[0])
+        else:
+            h_ave = fftconvolve(h, kernel, mode='same')
+        
+        # Computing norm using fast numba-compiled function
+        h_ave_norm_sq = _fast_norm_sq(h_ave)
+        h_norm_sq = _fast_norm_sq(h)
+        
+        # Avoid division by zero
+        if h_norm_sq > 0.0:
+            SD = h_ave_norm_sq / h_norm_sq
+        else:
+            SD = 0.0
+        
+        # Update h in-place using numba-compiled function
+        h = _fast_subtract(h, h_ave)
+        
+        if verbose:
+            print('(fft): %2.0d      %1.40f          %2.0d\n' % (inStepN, SD, np.size(a)))
+    
+    if not options.silent:
+        print('(fft): %2.0d      %1.40f          %2.0d\n' % (inStepN, SD, np.size(a)))
+
+    return h, inStepN, SD
 
 
 def compute_imf_fft_adv(f,a,options):
@@ -414,11 +513,13 @@ def compute_imf_fft_adv(f,a,options):
     MaxInner = options.MaxInner
     BCmod = options.BCmode
     NumSteps = options.NumSteps if 'NumSteps' in options else 1 
+    
         
     inStepN = 0
     SD = 1.
     
     Nh = len(h)
+    
     if BCmod == 'wrap':
         ker = kernel
         if f.shape[0] <ker.shape[0]:
@@ -440,6 +541,28 @@ def compute_imf_fft_adv(f,a,options):
             SD = np.sum(np.abs(fft_h_new-fft_h_old)**2)/np.sum(np.abs(fft_h_old)**2)
             if options.verbose:
                 print('(fft): %2.0d      %1.40f          %2.0d\n' % (inStepN, SD, np.size(a)))
+        # Binary search refinement: only if we overshot (SD <= delta)
+        if SD <= delta and NumSteps > 1:
+            lo = inStepN - NumSteps + 1   # start of the last coarse step
+            hi = inStepN                  # end of the last coarse step
+
+            while lo <= hi:
+                mid = (lo + hi) // 2
+
+                fft_h_old = (1 - kpad_fft) ** (mid - 1) * h_fft
+                fft_h_new = (1 - kpad_fft) ** mid        * h_fft
+                SD_mid = np.sum(np.abs(fft_h_new - fft_h_old) ** 2) / np.sum(np.abs(fft_h_old) ** 2)
+
+                if SD_mid <= delta:
+                    inStepN = mid    # candidate: try to go lower
+                    hi = mid - 1
+                else:
+                    lo = mid + 1     # need more steps
+
+            SD = np.sum(np.abs((1 - kpad_fft) ** inStepN       * h_fft -
+                               (1 - kpad_fft) ** (inStepN - 1) * h_fft) ** 2) \
+               / np.sum(np.abs((1 - kpad_fft) ** (inStepN - 1) * h_fft) ** 2)
+        
         h = np.fft.irfft(fft_h_new,n=f.shape[0])
     
     else:
@@ -478,7 +601,7 @@ def compute_imf_fft_adv(f,a,options):
 #        h = np.fft.irfft(fft_h_new,n=fftshape)[startind:endind]
 
     
-    if options.verbose:
+    if not options.silent:
         print('(fft): %2.0d      %1.40f          %2.0d\n' % (inStepN, SD, np.size(a)))
 
     return h,inStepN,SD
@@ -562,6 +685,36 @@ def compute_imf_fft_adv(f,a,options):
 #        print('(fft adv): %2.0d      %1.40f          %2.0d\n' % (inStepN, SD, np.size(a)))
 #
 #    return h,inStepN,SD
+def precompute_kernel_fft(ker, n):
+    m = ker.shape[0] // 2
+    kpad = np.pad(ker, (0, n - ker.shape[0]))
+    kpad = np.roll(kpad, -m)
+    return fft.rfft(kpad)   # store this
+
+def fftconvolve1D_kerfft(f,ker_fft,kershape):#, mode = 'same', BCmode = 'wrap'):
+    """
+    
+    Compute the 1D convolution between f and ker, using fft.
+    
+    It assumes that the field is periodic 
+
+    This function is used when the option "extend-periodic" is selected
+
+    
+    parameters
+    ----------
+    f : 1D-like array
+        input array
+    ker : 1D-like array
+        kernel of the convolution filter
+
+    """
+    if f.shape[0] <kershape:
+        print('error, kernel shape cannot be larger than 1D array shape')
+        return None
+    
+    return fft.irfft(fft.rfft(f) * ker_fft, n=f.shape[0])
+
 
 def fftconvolve1D(f,ker):#, mode = 'same', BCmode = 'wrap'):
     """

src/fifpy/MvIFpy.py
@@ -12,7 +12,7 @@
 
 
 from .IF_aux import *
-__version__='8.4'
+__version__='8.4.1'
 
 ################################################################################
 ###################### Mv Iterative Filtering main functions ###################
@@ -136,8 +136,8 @@ def MvIF(in_f,options,M=np.array([]), window_mask=None, data_mask = None, nthrea
     opts = AttrDictSens(options)
     silent = opts.silent
     if nthreads is not None:
-        if opts.imf_method == 'numba': 
-            set_num_threads(nthreads)
+        #if opts.imf_method == 'numba': 
+        set_num_threads(nthreads)
     if opts.verbose:
         print('running IF decomposition...')
         #if verbose:
@@ -150,6 +150,11 @@ def MvIF(in_f,options,M=np.array([]), window_mask=None, data_mask = None, nthrea
     if opts.imf_method == 'fft': 
         compute_imf = compute_imf_fft
         #compute_imf = compute_imf_fft_adv
+    elif opts.imf_method == 'fft_adv': 
+        compute_imf = compute_imf_fft_adv
+        #compute_imf = compute_imf_fft_adv
+    elif opts.imf_method == 'fft_numba': 
+        compute_imf = compute_imf_fft_numba
     elif opts.imf_method == 'numba': 
         compute_imf = compute_imf_numba
 

src/fifpy/__init__.py
@@ -285,6 +285,7 @@ class MvFIF(FIF):
         Return the extracted IMCs (excluding the extension)
         """
         return self.data['IMC'][:,:,self.wsh:-self.wsh] if self.wsh >0 else self.data['IMC'] 
+    
 
     def get_freq_amplitudes(self, as_output = False, use_instantaneous_freq = True,  **kwargs):
         """
@@ -317,12 +318,49 @@ class MvFIF(FIF):
                      **kwargs)
             freqs[i] = freqt
             amps[i] = ampt
-        self.data['freqs'] = freqs 
-        self.data['amps'] = amps
+        self.data['freqs'] = freqs#.transpose(1,0,2) 
+        self.data['amps'] = amps#.transpose(1,0,2)
         self.ancillary['get_freq_amplitudes'] = kwargs
         self.ancillary['get_freq_amplitudes']['use_instantaneous_freq'] = use_instantaneous_freq
         
         if as_output: return self.data['freqs'], self.data['amps']
+
+    def get_instantaneous_freq_amplitude(self, dt,as_output = False,**kwargs):
+        """
+        Calculates the instantaneous frequencies and amplitudes of the IMCs.
+        
+        Parameters
+        ----------
+        as_output : bool
+            if set to True, then it returns inst. frequency and amplitude.
+            If set to false, save the result in self.data['freqs'] and self.data['amps'].   
+        
+        the available **kwargs are the following kwargs from self.fif_tools.IMC_get_freq_amplitudes
+            dt : float (default = 1.)
+                time resolution (inverse of the sampling frequency)
+            resort : Bool (default = False)
+                if true, frequencies and amplitudes are sorted frequency-wise
+        """
+        wsh = self.ancillary['wshrink']
+        
+        nimf,ndim,nt = self.data['IMC'].shape
+        freqs = np.zeros((ndim,nimf,nt))
+        amps = np.zeros((ndim,nimf,nt))
+        for i in range(ndim):
+            freqt,ampt = ftools.IMC_get_inst_freq_amp(self.data['IMC'][:,i,:], dt=dt)
+            freqs[i] = freqt[...]
+            amps[i] = ampt[...]
+
+        self.data['I_freqs'] = freqs.transpose(1,0,2) 
+        self.data['I_amps'] = amps.transpose(1,0,2)
+        
+        setattr(self.__class__, 'I_freqs', _make_shrink_property('I_freqs'))
+        setattr(self.__class__, 'I_amps',  _make_shrink_property('I_amps')) 
+        #self.I_freqs = _make_shrink_property('I_freqs')
+        #self.I_amps = _make_shrink_property('I_amps')
+        if as_output: return self.data['freqs'], self.data['amps']
+
+    #I_freq2 = _make_shrink_property('I_freqs')
 
     def orthogonalize(self,threshold = 0.6, only_nearest = True, **kwargs):
         """
@@ -505,7 +543,7 @@ class MvIF(MvFIF):
             if not silent: print('\nmaking input signal periodic...')
             from .arrays import make_periodic
             
-            if wshrink == 0 : wshrink = in_f.size//4 
+            if wshrink == 0 : wshrink = in_f.shape[-1]//4 
             
             out_f = np.zeros((D,N)) 
             for iD in range(D):    
@@ -677,3 +715,11 @@ class MIF():
         imfs = ftools.orthogonalize(IMCs,threshold, only_nearest, **kwargs)
         self.ancillary['orthogonalized'] = True
         self.data['IMC'] = imfs
+
+
+def _make_shrink_property(key):
+    @property
+    def prop(self):
+        d = self.data[key]
+        return d[:, :, self.wsh:-self.wsh] if self.wsh > 0 else d
+    return prop